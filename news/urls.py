8b1fffd7c3d57a22815dbdbdb9a5a137d2224ad1
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2023-08-28 15:03:28 -0300

    Requisito 05

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from news.views import home
+from news.views import home, news_details
 
 
 urlpatterns = [
     path("", home, name='home-page'),
+    path("news/<int:id>/", news_details, name="news-details-page"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from news.models import News
 
@@ -6,3 +6,8 @@ from news.models import News
 def home(request):
     news = News.objects.all()
     return render(request, 'home.html', {'news': news})
+
+
+def news_details(request, id):
+    news = get_object_or_404(News, id=id)
+    return render(request, 'news_details.html', {'news': news})
