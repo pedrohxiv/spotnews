b102d89b7246b28f039e5a136b30af238201ec12
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2023-08-28 16:43:19 -0300

    Requisitos 08, 09 e 10

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -18,10 +18,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+from news_rest.views.categories_view import CategoriesViewSet
+from news_rest.views.users_view import UsersViewSet
+from news_rest.views.news_view import NewsViewSet
+
+router = DefaultRouter()
+router.register(r"categories", CategoriesViewSet)
+router.register(r"users", UsersViewSet)
+router.register(r"news", NewsViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('news.urls'))
+    path("", include('news.urls')),
+    path("api/", include(router.urls))
 ]
 
 if settings.DEBUG:
